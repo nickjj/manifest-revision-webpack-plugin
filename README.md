a3a2fa531e352eea1b0527711128e3a2e6153fb2
Nick Janetakis <nick.janetakis@gmail.com>
2015-05-04 07:23:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+## Manifest revision plugin for webpack
+
+Wouldn't it be nice if you could output a json file that looked like this?
+
+```json
+{
+  "publicPath": "http://localhost:2992/assets/",
+  "assetsByChunkName": {
+    "app_js": "app_js.b359b8f956c6cfd0c7f2.js",
+    "app_css": [
+      "app_css.9b34bf5794798118d77c.js",
+      "app_css.9b34bf5794798118d77c.css"
+    ],
+    "common": "common.43ba800be83617a071ca.js"
+  },
+  "assets": {
+    "images/hamburger.svg": "images/hamburger.d2cb0dda3e8313b990e8dcf5e25d2d0f.svg",
+    "images/touch/320x600__light-aqua.png": "images/touch/320x600__light-aqua.f0d51db7c4aa72532cf26fe10a616a0f.png",
+    "images/touch/apple-touch-icon.png": "images/touch/apple-touch-icon.7326f54bfe6776293f08b34c3a5fde7b.png",
+    "images/touch/chrome-touch-icon-192x192.png": "images/touch/chrome-touch-icon-192x192.571f134f59f14a6d298ddd66c015b293.png",
+    "images/touch/icon-128x128.png": "images/touch/icon-128x128.7c46d686765c49b813ac5eb34fabf712.png",
+    "images/touch/ms-touch-icon-144x144-precomposed.png": "images/touch/ms-touch-icon-144x144-precomposed.452d90b250d6f41a0c8f9db729113ffd.png"
+  }
+}
+```
+
+Well, now you can. Here's what it includes:
+
+- Assets include their logical paths for easy lookups on your server
+- You only have to keep track of your asset server in 1 place (your webpack config)
+- It's as small as possible and provides just enough info to do it's intended to do
+
+## Installation
+
+`npm install --save manifest-revision-webpack-plugin`
+
+
+## Quick start
+
+```js
+var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
+
+// This is where we want the manifest.json file to be created.
+var manifestPath = path.join(__dirname, 'build', 'manifest.json');
+
+// Where are your assets located in your project? This would typically be a path
+// that contains folders such as: images, stylesheets and javascript.
+var rootAssetPath = './src/client';
+
+module.exports = {
+  plugins: [
+    new ManifestRevisionPlugin(manifestPath, rootAssetPath)
+  ]
+};
+```
+
+## How would I use the manifest file?
+
+It can be used with any server and any programming language. What you could do
+is read the `manifest.json` file in once when your app boots up. Then create a
+template helper that accepts an asset and behind the scenes it will lookup
+that asset and return back the real file name.
+
+### What would that look like on the server?
+
+Ok, let's say you're using the jinja template engine. You've also created a
+helper called `asset_for`.
+
+##### Call your template helper in a template:
+```jinja
+<img src="{{ asset_for('images/hamburger.svg') }}" alt="Hamburger">
+```
+
+##### It will produce this source code:
+```jinja
+<img src="images/hamburger.d2cb0dda3e8313b990e8dcf5e25d2d0f.svg" alt="Hamburger">
+```
+
+### What would the implementation of that look like?
+
+There's too many web frameworks to include examples. As people use the project
+it would be nice to create wiki pages that have end to end examples on how to
+implement it in popular web frameworks.
+
+## Contributors
+
+- Nick Janetakis <<nick.janetakis@gmail.com>>
---
+++ index.js
@@ -0,0 +1,91 @@
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * Produce a much slimmer version of webpack stats containing only information
+ * that you would be interested in when creating an asset manifest.
+
+ * @param {string} output - The output file path.
+ * @param {string} rootAssetPath - The root path of where your assets are.
+ */
+var ManifestRevisionPlugin = function (output, rootAssetPath) {
+    this.output = output;
+    this.rootAssetPath = rootAssetPath;
+};
+
+/**
+ * When given a logical asset path, produce an array that contains the logical
+ * path of the asset without the cache hash included as the first element.
+ * The second element contains the cached version of the asset name.
+
+ * @param {string} logicalAssetPath - The path of the asset without the root.
+ * @param {string} cachedAsset - The file name of the asset with the cache hash.
+ * @returns {Array}
+ */
+ManifestRevisionPlugin.prototype.mapAsset = function (logicalAssetPath, cachedAsset) {
+    var logicalPath = logicalAssetPath.substring(0,
+        logicalAssetPath.lastIndexOf('/'));
+
+    return [logicalAssetPath, path.join(logicalPath, cachedAsset)];
+};
+
+/**
+ * Take in the modules array from the webpack stats and produce an object that
+ * only includes assets that matter to us. The key is the asset logical path
+ * and the value is the logical path but with the cached asset name.
+ *
+ * You would use this as a lookup table in your web server.
+
+ * @param {string} data - Array of webpack modules.
+ * @returns {Object}
+ */
+ManifestRevisionPlugin.prototype.parsedAssets = function (data) {
+    var assets = {};
+
+    for (var i = 0, length = data.length; i < length; i++) {
+        var item = data[i];
+
+        // Attempt to ignore chunked assets and other unimportant assets.
+        if (item.name.indexOf('multi ') === -1 &&
+            item.name.indexOf('~/') === -1 &&
+            item.reasons.length == 0) {
+
+            var nameWithoutRoot = item.name.replace(this.rootAssetPath, '');
+            var mappedAsset = this.mapAsset(nameWithoutRoot, item.assets[0]);
+
+            assets[mappedAsset[0]] = mappedAsset[1];
+        }
+    }
+
+    return assets;
+};
+
+ManifestRevisionPlugin.prototype.apply = function (compiler) {
+    var self = this;
+    var output = this.output;
+
+    // Micro optimize toJson by eliminating all of the data we do not need.
+    var options = {};
+    options.assets = true;
+    options.version = false;
+    options.timings = false;
+    options.chunks = false;
+    options.chunkModules = false;
+    options.cached = true;
+    options.source = false;
+    options.errorDetails = false;
+    options.chunkOrigins = false;
+
+    compiler.plugin('done', function (stats) {
+        var importantStats = {};
+        var data = stats.toJson(options);
+
+        importantStats.publicPath = data.publicPath;
+        importantStats.assetsByChunkName = data.assetsByChunkName;
+        importantStats.assets = self.parsedAssets(data.modules);
+
+        fs.writeFileSync(output, JSON.stringify(importantStats));
+    });
+};
+
+module.exports = ManifestRevisionPlugin;
